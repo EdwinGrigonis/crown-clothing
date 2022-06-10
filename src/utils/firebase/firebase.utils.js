1c87be7d790494dfce92dc1c20b265dab753562e
Edwin Grigonis <edwin.grigonis2@mail.dcu.ie>
2022-06-10 14:47:29 +0900

    added utils folder

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBWzrUmN9RLTyakEUmzH7UWpQJQMBPTJ4M",
+    authDomain: "crown-clothing-db-70f64.firebaseapp.com",
+    projectId: "crown-clothing-db-70f64",
+    storageBucket: "crown-clothing-db-70f64.appspot.com",
+    messagingSenderId: "3824137144",
+    appId: "1:3824137144:web:68596919ee4c2af9c8554d"
+  };
+  
+  // Initialize Firebase
+  const firebaseApp = initializeApp(firebaseConfig);
