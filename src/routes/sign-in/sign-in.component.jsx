cba99e7d4f9a037717dcdb2cbdf6e51e7c30c4a5
Edwin Grigonis <edwin.grigonis2@mail.dcu.ie>
2022-06-14 16:28:50 +0900

    refined auth

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,6 +1,11 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils" 
+import { 
+    auth,
+    signInWithGooglePopup, 
+    createUserDocumentFromAuth
+ } from "../../utils/firebase/firebase.utils" 
 
 const SignIn = () => {
+
     const logGoogleUser = async () => {
         const {user} = await signInWithGooglePopup();
         const userDocRef = await createUserDocumentFromAuth(user);
@@ -8,7 +13,7 @@ const SignIn = () => {
 
     return (
         <div>
-            <h1>Sign in PAge</h1>
+            <h1>Sign in Page</h1>
             <button onClick={logGoogleUser}>
                 Sign in with Google Popup
             </button>
