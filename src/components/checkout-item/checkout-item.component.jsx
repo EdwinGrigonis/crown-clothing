7dfbdf063cde23065fcc941d454661b1dd7936ce
Edwin Grigonis <edwin.grigonis2@mail.dcu.ie>
2022-06-29 17:49:30 +0900

    added total

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -21,7 +21,7 @@ return (
             <span className='value'>{quantity}</span>
             <div className='arrow' onClick={addItemHandler}>&#10095;</div>
         </span>
-        <span className='price'> {price} </span>
+        <span className='price'> ${price} </span>
         <strong className='remove-button' onClick={clearItemHandler}>&#10005;</strong>
     </div>
 )
--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -45,6 +45,7 @@ export const CartContext = createContext({
     removeItemFromCart: () => {},
     clearItemFromCart: () => {},
     cartCount: 0,
+    cartTotal: 0,
 
 })
 
@@ -52,6 +53,7 @@ export const CartProvider = ({children}) => {
     const [isCartOpen, setIsCartOpen] = useState(false);
     const [cartItems, setCartItems] = useState([]);
     const [cartCount, setCartCount] = useState(0);
+    const [cartTotal, setCartTotal] = useState(0);
 
     useEffect(() => {
         const newCartCount = cartItems.reduce(
@@ -59,6 +61,14 @@ export const CartProvider = ({children}) => {
             0
         );
         setCartCount(newCartCount);
+    }, [cartItems]);
+
+    useEffect(() => {
+        const newCartTotal = cartItems.reduce(
+            (total, cartItem) => total + cartItem.quantity * cartItem.price,
+            0
+        );
+        setCartTotal(newCartTotal);
     }, [cartItems]);
 
     const addItemToCart = (productToAdd) => {
@@ -80,6 +90,8 @@ export const CartProvider = ({children}) => {
                    removeItemToCart,
                    clearItemFromCart,
                    cartItems, 
-                   cartCount}
+                   cartCount,
+                   cartTotal,
+                }
     return (<CartContext.Provider value={value}>{children}</CartContext.Provider>)
 }
