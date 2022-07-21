1660024cd9c2f23541a1d4d8ee3c374fb10e6cdb
Edwin Grigonis <edwin.grigonis2@mail.dcu.ie>
2022-07-21 15:30:28 +0900

    started cart reducer

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -49,6 +49,27 @@ export const CartContext = createContext({
 
 })
 
+const INITIAL_STATE = {
+    isCartOpen: false,
+    cartItems: [],
+    cartCount: 0,
+    cartTotal: 0, 
+};
+
+const cartReducer = (state, action) => {
+    const { type, payload } = action;
+
+    switch(type) {
+        case 'SET_CART_ITEMS':
+        return {
+            ...state,
+            ...payload
+        }
+        default:
+            throw Error(`Unhandled type of ${type} in CartReducer`);
+    }
+}
+
 export const CartProvider = ({children}) => {
     const [isCartOpen, setIsCartOpen] = useState(false);
     const [cartItems, setCartItems] = useState([]);
